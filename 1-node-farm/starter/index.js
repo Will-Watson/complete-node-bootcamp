8c015767cf70ce906861fc40e02181e77b10ef13
William Watson <williampwatson1991@gmail.com>
2021-11-09 15:24:55 -0500

    error handling

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -11,6 +11,8 @@ const fs = require('fs');
 // Non-blocking, asychronous way
 
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  //if (err) return console.log('ERRORRRRRR');
+
   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
     console.log(data2);
     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
