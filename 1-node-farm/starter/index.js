38ad7ceffa66a1be242c1fbad802a6ffc53b143f
William Watson <williampwatson1991@gmail.com>
2021-11-09 15:36:10 -0500

    section 2/11

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,4 +1,9 @@
 const fs = require('fs');
+const http = require('http');
+
+/* =========================================================== */
+
+//FILES
 
 // Blocking, synchronous way
 
@@ -10,19 +15,30 @@ const fs = require('fs');
 
 // Non-blocking, asychronous way
 
-fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
-  //if (err) return console.log('ERRORRRRRR');
+//fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+//if (err) return console.log('ERRORRRRRR');
 
-  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
-    console.log(data2);
-    fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
-      console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+//     console.log(data2);
+//     fs.readFile(`./txt/append.txt`, 'utf-8', (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
-        console.log('Your file has been written! HOLLLEERRRRR');
-      });
-    });
-  });
+//       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+//         console.log('Your file has been written! HOLLLEERRRRR');
+//       });
+//     });
+//   });
+// });
+
+// console.log('Will read file!');
+
+/* =========================================================== */
+// SERVER
+
+const server = http.createServer((req, res) => {
+  res.end('Hello from the server');
 });
 
-console.log('Will read file!');
+server.listen(8000, '127.0.0.1', () => {
+  console.log('Listening to requests on port 8000');
+});
