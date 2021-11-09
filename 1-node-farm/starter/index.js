d7a67861631290268a6abe47f455c4699aa95c01
William Watson <williampwatson1991@gmail.com>
2021-11-09 16:59:25 -0500

    section: 2/13

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -58,6 +58,8 @@ const server = http.createServer((req, res) => {
   }
 });
 
+//console.log(dataObj);
+
 server.listen(8000, '127.0.0.1', () => {
   console.log('Listening to requests on port 8000');
 });
