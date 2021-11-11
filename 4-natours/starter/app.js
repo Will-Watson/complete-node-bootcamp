11368e2deded57335bfb92e90df373fda6464707
William Watson <williampwatson1991@gmail.com>
2021-11-10 22:17:25 -0500

    file order

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,17 +1,34 @@
 const fs = require('fs');
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 
+//middlewares
+app.use(morgan('dev'));
 app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log('Hello from the middleware');
+  next();
+});
+
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
 
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
+//route handlers
+
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt: req.requestTime,
     results: tours.length,
     data: {
       tours,
@@ -100,6 +117,7 @@ app
   .patch(updateTour)
   .delete(deleteTour);
 
+//server
 const port = 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}....`);
