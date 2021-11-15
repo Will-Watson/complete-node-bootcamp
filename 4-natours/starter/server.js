a63c72c668a3a02f793ef4edb2547fb75ca55096
William Watson <williampwatson1991@gmail.com>
2021-11-15 11:18:52 -0500

    sectoin: 8/82

--- 4-natours/starter/server.js
+++ 4-natours/starter/server.js
@@ -1,6 +1,26 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
 const app = require('./app');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE;
+//below commented out code used a replace method for security purposes replaceing the <PASSWORD> string with the process.env.PASSWORD but for some reason didn't work correctly so I entered the password manually.
+// .replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASWORD
+// );
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('DB connection successful');
+  });
 
 //server
 const port = process.env.PORT || 3000;
