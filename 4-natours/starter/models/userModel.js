64578b0f69251fdd97d29140c2f9bc7ca0581c71
William Watson <williampwatson1991@gmail.com>
2021-11-17 15:57:29 -0500

    section: 10/126

--- 4-natours/starter/models/userModel.js
+++ 4-natours/starter/models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -25,11 +26,25 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: true,
     validate: {
+      //This only works on CREATE and SAVE!!
       validator: function (value) {
         return value === this.password;
       },
+      message: 'Passwords do not match',
     },
   },
+});
+
+userSchema.pre('save', async function (next) {
+  // Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  // Hash the password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  // Delete password confirm field
+  this.passwordConfirm = undefined;
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
