2016988d00bcfbd0bbb40720a6c0d6949f58fb7c
William Watson <williampwatson1991@gmail.com>
2021-11-22 13:31:18 -0500

    section 10 complete

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
+const hpp = require('hpp');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -10,9 +14,16 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 //middlewares
+// Set security http headers
+app.use(helmet());
+
+//development logging
+
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+//rate limiter, limiting API requests
 
 const limiter = rateLimit({
   max: 100,
@@ -22,8 +33,35 @@ const limiter = rateLimit({
 
 app.use('/api', limiter);
 
-app.use(express.json());
+// Body parser, reading data from body
+
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitazation agains NoSQL query injection
+app.use(mongoSanitize());
+
+// Data sanitaization against XSS
+app.use(xss());
+
+// Prevent parameter polluction
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAvergae',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  })
+);
+
+//serving static files
+
 app.use(express.static(`${__dirname}/public`));
+
+//test middleware
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
