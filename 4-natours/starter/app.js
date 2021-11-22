b9565a87de76e48ad4299dbe31a4fb2d37b43675
William Watson <williampwatson1991@gmail.com>
2021-11-22 13:10:43 -0500

    section: 10/142

--- 4-natours/starter/app.js
+++ 4-natours/starter/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
@@ -13,12 +14,20 @@ if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP please try again in one hour',
+});
+
+app.use('/api', limiter);
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
-  console.log(req.headers);
+  //console.log(req.headers);
 
   next();
 });
