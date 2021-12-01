e7dc14552394b41a22b4d5168c03154152532b9b
William Watson <williampwatson1991@gmail.com>
2021-12-01 14:15:39 -0500

    section: 11/154

--- 4-natours/starter/models/reviewModel.js
+++ 4-natours/starter/models/reviewModel.js
@@ -33,6 +33,11 @@ const reviewSchema = new mongoose.Schema(
   }
 );
 
+reviewSchema.pre(/^find/, function (next) {
+  this.populate('user', 'name').populate('tour', 'name');
+  next();
+});
+
 const Review = mongoose.model('Review', reviewSchema);
 
 module.exports = Review;
