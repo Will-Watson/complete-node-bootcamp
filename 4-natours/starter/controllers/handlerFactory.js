e7bad4bd73158a125d9eadb42cada9355f58fd6f
William Watson <williampwatson1991@gmail.com>
2021-12-01 17:12:03 -0500

    section: 11/162

--- 4-natours/starter/controllers/handlerFactory.js
+++ 4-natours/starter/controllers/handlerFactory.js
@@ -1,5 +1,6 @@
 const catchAsync = require('./../utils/catchAsync');
 const AppError = require('./../utils/appError');
+const APIFeatures = require('./../utils/apiFeatuers');
 
 exports.deleteOne = (Model) =>
   catchAsync(async (req, res, next) => {
@@ -40,6 +41,47 @@ exports.createOne = (Model) =>
 
     res.status(201).json({
       status: 'success',
+      data: {
+        data: document,
+      },
+    });
+  });
+
+exports.getOne = (Model, popOptions) =>
+  catchAsync(async (req, res, next) => {
+    let query = Model.findById(req.params.id);
+    if (popOptions) query = query.populate(popOptions);
+
+    const document = await query;
+
+    if (!document) {
+      return next(new AppError('No document found with that ID', 404));
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: {
+        document,
+      },
+    });
+  });
+
+exports.getAll = (Model) =>
+  catchAsync(async (req, res, next) => {
+    // To allow for nested get reviews on tour (hack)
+    let filter = {};
+    if (req.params.tourId) filter = { tour: req.params.tourId };
+
+    const features = new APIFeatures(Model.find(filter), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const document = await features.query;
+
+    res.status(200).json({
+      status: 'success',
+      results: document.length,
       data: {
         data: document,
       },
